99380aac1c4506753644471e1c7c6212af85db48
NivekCreator <128836474+NivekCreator@users.noreply.github.com>
2023-05-24 19:18:20 -0400

    Add files via upload

---
+++ Web.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template_string, session
+import openai
+
+app = Flask(__name__)
+app.secret_key = "5404"
+openai.api_key = "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"
+
+@app.route('/')
+def index():
+    session['conversation'] = []
+    return '''
+    <form method="POST" action="/get">
+        <input type="text" name="user_input">
+        <input type="submit" value="Enviar">
+    </form>
+    '''
+
+@app.route('/get', methods=['POST'])
+def get_bot_response():
+    user_input = request.form['user_input']
+    conversation = session.get('conversation', [])
+    conversation.append(f"User: {user_input}")
+
+    conversation_str = "\n".join(conversation)
+    prompt = f"{conversation_str}\nAI:"
+    
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    response_text = response.choices[0].text.strip()
+    conversation.append(f"AI: {response_text}")
+    session['conversation'] = conversation
+
+    return render_template_string('''
+    <form method="POST" action="/get">
+        <input type="text" name="user_input">
+        <input type="submit" value="Enviar">
+    </form>
+    <p>{{ response_text }}</p>
+    ''', response_text=response_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
